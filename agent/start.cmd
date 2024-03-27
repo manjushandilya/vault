1f07785ec58d24d4bf9f241e9c6f0a03bb44786e
Manjunath Sathyanarayana <63997896+manjushandilya@users.noreply.github.com>
2024-03-27 18:47:18 +0530

    Update start.cmd

--- agent/start.cmd
+++ agent/start.cmd
@@ -2,14 +2,15 @@ set VAULT_ADDR=http://localhost:8200
 
 vault auth enable approle
 
-vault policy write my-role-policy policy.cnf 
+vault policy write esbrole-policy policy.cnf 
 
-vault write auth/approle/role/my-role token_policies="my-role-policy"
+vault write auth/approle/role/esbrole token_policies="esbrole-policy" ^
+bind_secret_id=false secret_id_bound_cidrs="0.0.0.0/0"
 
-vault read -field=role_id auth/approle/role/my-role/role-id > role_id_file
+vault read -field=role_id auth/approle/role/esbrole/role-id > role_id_file
 
-vault write -field=secret_id -f auth/approle/role/my-role/secret-id > secret_id_file
+REM vault write -field=secret_id -f auth/approle/role/esbrole/secret-id > secret_id_file
 
-vault kv put secret/integrationserver foo=bar
+vault kv put secret/esb foo=bar
 
 vault agent -config=config.hcl -log-level=debug
